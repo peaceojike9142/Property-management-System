b0d314c722e0ea589dfab4cad29006bff16e19e4
Peace Ojike <peaceojike9142@gmail.com>
2026-06-19 17:11:05 +0100

    login to dashboard

---
+++ FrontEnd/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+
+            <h2>Welcome Peace 🚀</h2>
+
+            <p>Property Management System</p>
+        </div>
+    );
+}
+
+export default Dashboard;
